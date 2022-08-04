1ea70ebde03af1a16475a16293f0d67e5617df24
manotz <guillertalacoro.az711224@gmail.com>
2022-08-04 08:41:33 -0500

    subiendo proyecto

---
+++ src/hooks/usePacientes.jsx
@@ -0,0 +1,13 @@
+import { useContext } from 'react';
+import PacientesContext from '../context/PacientesProvider';
+
+const usePacientes = () =>{
+
+    return useContext(PacientesContext);
+
+}
+
+export default usePacientes;
+
+
+
